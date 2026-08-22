d7029a9bf1e1b87e78543476e4e7a38002555a9c
Wellington Pragidi <wellingtonpragidi@gmail.com>
2026-08-21 22:27:30 -0300

    Corrigido a formatação da declaração de chave primária no scheme.sql
    Havia uma vírgula depois de PRIMARY KEY (ID) na tabela statistics.

--- install/scheme.sql
+++ install/scheme.sql
@@ -141,7 +141,7 @@ CREATE TABLE statistics (
     created date NOT NULL,
     period time NOT NULL,
     IP varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,   
-    PRIMARY KEY (ID),
+    PRIMARY KEY (ID)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 
